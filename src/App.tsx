045622efc0f79d4366aefc29fb2001adb54094ae
Poto5qin <160687728+Poto5qin@users.noreply.github.com>
2025-06-26 22:12:42 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,5 @@
 //   const linkElement = screen.getByText(/learn react/i);
 //   expect(linkElement).toBeInTheDocument();
 // });
-import React from 'react';
-import { schedule, getCalendarDataFromSchedule } from './Module/General'; // adjust path if needed
-import { MyCalender } from './containers/calender'; 
 
-export default function App() {
-  const calendarData = getCalendarDataFromSchedule(schedule);
-  return (
-    <div>
-      <h1>NTU Hackathon Calendar</h1>
-      <Calender data={calendarData} />
-    </div>
-  );
-}
 
