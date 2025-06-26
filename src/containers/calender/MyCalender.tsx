7a306848958bf1e0104e4e345295ed66ed03b464
Poto5qin <160687728+Poto5qin@users.noreply.github.com>
2025-06-26 22:21:15 +0800

    Update MyCalender.tsx

--- src/containers/calender/MyCalender.tsx
+++ src/containers/calender/MyCalender.tsx
@@ -1,8 +1,1 @@
-import React from 'react';
-import { schedule, getCalendarDataFromSchedule } from '../../Module/General';
-import { Calender } from './index'; // index.tsx exports your Calender component
 
-export default function MyCalender(props) {
-  const calendarData = getCalendarDataFromSchedule(schedule);
-  return <Calender data={calendarData} {...props} />;
-}
